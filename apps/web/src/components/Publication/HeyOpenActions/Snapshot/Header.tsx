fd5bf9d1f76651bf071a478890a860432ea810c4
nikomatt69 <nicola.mattioli.95@gmail.com>
2023-11-28 21:10:49 +0100

    0.0.11

--- apps/web/src/components/Publication/HeyOpenActions/Snapshot/Header.tsx
+++ apps/web/src/components/Publication/HeyOpenActions/Snapshot/Header.tsx
@@ -5,6 +5,7 @@ import cn from '@lensshare/ui/cn';
 import Link from 'next/link';
 import type { FC } from 'react';
 import { Image } from '@lensshare/ui';
+import urlcat from 'urlcat';
 interface HeaderProps {
   proposal: Proposal;
 }
@@ -14,38 +15,39 @@ const Header: FC<HeaderProps> = ({ proposal }) => {
   const spaceUrl = `${SNAPSHOT_URL}/#/${space?.id}`;
 
   return (
-    <div className="flex- mb-2 items-center space-x-1 text-xs">
-      <div
-        className={cn(
-          state === 'active' ? 'bg-green-500' : 'bg-brand-500',
-          'mr-1 rounded-full px-2 py-0.5 text-xs capitalize text-white'
-        )}
-      >
-        {state}
+    <>
+      <div className="mb-2 flex items-center space-x-1 text-sm">
+        <div
+          className={cn(
+            state === 'active' ? 'bg-green-500' : 'bg-brand-500',
+            'mr-1 rounded-full px-2 py-0.5 text-xs capitalize text-white'
+          )}
+        >
+          {state}
+        </div>
+        <Image
+          src={urlcat('https://cdn.stamp.fyi/space/:address', {
+            address: space?.id
+          })}
+          className="mr-1 h-5 w-5 rounded-full"
+          alt={space?.id}
+        />
+        <Link href={spaceUrl} className="font-bold" target="_blank">
+          {space?.name ?? space?.id}
+        </Link>
+        <span>by</span>
+        <Link href={`${SNAPSHOT_URL}/#/profile/${author}`} target="_blank">
+          {formatAddress(author)}
+        </Link>
       </div>
-      <Image
-        src={`https://cdn.stamp.fyi/space/${space?.id}`}
-        className="mr-1 h-5 w-5 rounded-full"
-        alt={space?.id}
-      />
-      <Link href={spaceUrl} className=" text-xs" target="_blank">
-        {space?.name ?? space?.id}
-      </Link>
-      <span>by</span>
-      <Link href={`${SNAPSHOT_URL}/#/profile/${author}`} target="_blank">
-        {formatAddress(author)}
-      </Link>
-
       <Link
         href={`${spaceUrl}/proposal/${id}`}
-        className="line-clamp-5  text-xs"
+        className="font-bold"
         target="_blank"
       >
-        <div className="line-clamp-5 break-words  text-xs">
-          {title}
-        </div>
+        {title}
       </Link>
-    </div>
+    </>
   );
 };
 
